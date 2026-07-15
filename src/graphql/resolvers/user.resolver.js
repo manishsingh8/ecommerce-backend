c4699b9c122a21c43df55eda1adec3fe4e586e8d
Manish Singh <manishsingh04031998@gmail.com>
2026-07-15 16:42:40 +0530

    generate-otp-resolved

--- src/graphql/resolvers/user.resolver.js
+++ src/graphql/resolvers/user.resolver.js
@@ -1,7 +1,7 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../../models/User");
-const generateOTP = require("../../utils/temp");
+const generateOTP = require("../../utils/generateOTP");
 
 const generateToken = (user) => {
   return jwt.sign(
